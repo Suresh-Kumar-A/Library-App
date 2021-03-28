40dc9774371dd5ce5db019cc49c59c135b5c8671
A.Suresh Kumar <creativegeeksuresh@gmail.com>
2021-03-28 20:09:50 +0530

    Removed api fecting of books & replaced with Thymleaf

--- src/main/java/com/gmail/creativegeeksuresh/libraryapp/controller/AppController.java
+++ src/main/java/com/gmail/creativegeeksuresh/libraryapp/controller/AppController.java
@@ -6,7 +6,6 @@ import com.gmail.creativegeeksuresh.libraryapp.service.UserService;
 import com.gmail.creativegeeksuresh.libraryapp.service.util.CustomUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +13,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
-
-    @Value("${default.storage.path}")
-    private String defaultStoragePath;
 
     @Autowired
     UserService userService;
@@ -40,54 +36,37 @@ public class AppController {
         return new ModelAndView("create-account");
     }
 
-    @GetMapping(value = "/test")
-    public ModelAndView testPage() {
-        ModelAndView mv = new ModelAndView("test");
-        // Path path = Paths.get("E:\\adobedata.pdf").toAbsolutePath().normalize();
-
-        // Resource resource=null;
-        // try {
-        // resource = new UrlResource(path.toUri());
-        // } catch (Exception e) {
-        // // TODO: handle exception
-        // }
-        // mv.addObject("resource", resource);
-        return mv;
-    }
-
     @GetMapping(value = "/user/view-books")
     public ModelAndView viewBooksUserPage() {
         ModelAndView mv = new ModelAndView("user/view-books");
-        mv.addObject("username", customUtils.getUsername());
+        mv.addObject("bookList", bookService.getAllBooks());
         return mv;
     }
 
     @GetMapping(value = "/admin/dashboard")
     public ModelAndView adminDashboardPage() {
         ModelAndView mv = new ModelAndView("admin/dashboard");
-        mv.addObject("username", customUtils.getUsername());
         return mv;
     }
 
     @GetMapping(value = "/admin/view-users")
     public ModelAndView viewUsersPage() {
         ModelAndView mv = new ModelAndView("admin/view-users");
-        mv.addObject("users", userService.getAllUsers());
-        mv.addObject("username", customUtils.getUsername());
+        mv.addObject("userList", userService.getAllUsers());
         return mv;
     }
 
     @GetMapping(value = "/admin/view-books")
     public ModelAndView viewBooksPage() {
         ModelAndView mv = new ModelAndView("admin/view-books");
-        mv.addObject("username", customUtils.getUsername());
+        mv.addObject("bookList", bookService.getAllBooks());
         return mv;
     }
 
     @GetMapping(value = "/admin/add-book")
     public ModelAndView addBookPage() {
         ModelAndView mv = new ModelAndView("admin/add-book");
-        mv.addObject("username", customUtils.getUsername());
+
         return mv;
     }
 
@@ -95,7 +74,7 @@ public class AppController {
     public ModelAndView editBookPage(@RequestParam String uid) {
         ModelAndView mv = new ModelAndView("admin/edit-book");
         mv.addObject("book", bookService.findByUid(uid));
-        mv.addObject("username", customUtils.getUsername());
+
         return mv;
     }
 
@@ -103,14 +82,14 @@ public class AppController {
     public ModelAndView unavailableBooksPage() {
         ModelAndView mv = new ModelAndView("admin/unavailable-books");
         mv.addObject("books", bookService.getAllUnavilableBooks());
-        mv.addObject("username", customUtils.getUsername());
+
         return mv;
     }
 
     @GetMapping(value = "/admin/users-book-request")
     public ModelAndView usersBookRequestPage() {
         ModelAndView mv = new ModelAndView("admin/users-book-request");
-        mv.addObject("username", customUtils.getUsername());
+
         mv.addObject("requestList", bookRequestService.getAllBooks());
         return mv;
     }
